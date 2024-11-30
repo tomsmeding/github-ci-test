67758803f21248df155498715fdbc4a7688cad0c
Tom Smeding <tom@tomsmeding.com>
2024-11-30 14:51:24 +0100

    Try building tracy inside accelerate?

--- ci-script.sh
+++ ci-script.sh
@@ -3,14 +3,28 @@ set -euo pipefail
 
 set -x
 clang --version
-# clang -Wall -O2 -march=native test.c -o test
-# ./test
-# c++ -Wall -O2 -march=native test.c -o test2
-# ./test2
 
-git clone https://github.com/wolfpld/tracy
-cd tracy
-git checkout v0.11.1
-mkdir build
-cmake -B build -S capture -DCMAKE_BUILD_TYPE=Release -DLEGACY=ON -DUSE_WAYLAND=OFF
-cmake --build build --config Release -j 2 -- VERBOSE=1
+if false; then
+  clang -Wall -O2 -march=native test.c -o test
+  ./test
+  c++ -Wall -O2 -march=native test.c -o test2
+  ./test2
+fi
+
+if false; then
+  git clone https://github.com/wolfpld/tracy
+  cd tracy
+  git checkout v0.11.1
+  mkdir build
+  cmake -B build -S capture -DCMAKE_BUILD_TYPE=Release -DLEGACY=ON -DUSE_WAYLAND=OFF
+  cmake --build build --config Release -j 2 -- VERBOSE=1
+fi
+
+{
+  git clone https://github.com/AccelerateHS/accelerate
+  cd accelerate
+  git submodule update --init --recursive
+  cd cbits/tracy
+  cmake -B build -S capture -DCMAKE_BUILD_TYPE=Release -DLEGACY=ON -DUSE_WAYLAND=OFF
+  cmake --build build --config Release -j 2 -- VERBOSE=1
+}
