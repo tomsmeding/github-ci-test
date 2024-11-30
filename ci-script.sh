95cef6a53dea91e14ae2d2d59e38a6a5ab816b87
Tom Smeding <tom@tomsmeding.com>
2024-11-30 12:22:32 +0100

    try tracy

--- ci-script.sh
+++ ci-script.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -x
 clang --version
-clang -Wall -O2 -march=native test.c -o test
-./test
-c++ -Wall -O2 -march=native test.c -o test2
-./test2
+# clang -Wall -O2 -march=native test.c -o test
+# ./test
+# c++ -Wall -O2 -march=native test.c -o test2
+# ./test2
+
+git clone https://github.com/wolfpld/tracy
+cd tracy
+mkdir build
+cmake -B build -S capture -DCMAKE_BUILD_TYPE=Release -DLEGACY=1
+cmake --build build --config Release -j 2 -- VERBOSE=1
